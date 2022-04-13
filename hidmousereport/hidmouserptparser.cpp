15f2612c9df3aed93fd5bebeb3b847eba8fe9783
wenliang lu <wenliang.lu@gmail.com>
2022-04-13 13:37:06 -0700

    arduino better switch key

--- hidmousereport/hidmouserptparser.cpp
+++ hidmousereport/hidmouserptparser.cpp
@@ -18,7 +18,7 @@ void HIDMouseReportParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8
   //       Serial.print(buf[i], DEC);
   //       Serial.print(",");
   // }
-  Serial.println("");
+  // Serial.println("");
   // Serial.println("");
 
   //but_id 1,2,4,8,16
@@ -62,30 +62,38 @@ void HIDMouseReportParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8
   }
 }
 
+char k = '1';
+void switch_key(char k2) {
+  if (k2 != k) {
+    Keyboard.write(k2);
+    k = k2;
+  }
+}
+
 void HIDMouseEvents::OnButtonDn(uint8_t but_id) {
 
   // for minecraft,  quickly switch weapon for each click
   if (1 & but_id) {
-    Keyboard.write('2');
+    switch_key('2');
     Mouse.press(MOUSE_RIGHT);
   } else if (2 & but_id) {
-    Keyboard.write('1');
+    switch_key('1');
     for (uint8_t i = 1; i <= 2; i++) {
       Mouse.click(MOUSE_LEFT);
       // 20ms does not work. too close.
-      // NOTE: not multi-thread. large number will block movement of mouse below.      
+      // NOTE: not multi-thread. large number will block movement of mouse below.
       // TODO: make it multi-thread?
-      delay(30);    
+      delay(30);
     }
     Mouse.press(MOUSE_LEFT);
   } else if (4 & but_id) {
-    Keyboard.write('4');
+    switch_key('4');
     Mouse.press(MOUSE_RIGHT);
   } else if (8 & but_id) {
-    Keyboard.write('3');
+    switch_key('3');
     Mouse.press(MOUSE_LEFT);
   } else if (16 & but_id) {
-    Keyboard.write('8');
+    switch_key('8');
     Mouse.press(MOUSE_RIGHT);
   }
 }
